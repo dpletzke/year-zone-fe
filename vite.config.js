6eaed5b995188c59aeda0980f36c4fc00cc044d8
dpletzke <dpletzke@gmail.com>
2023-08-28 10:15:07 -0500

    add chunking

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,19 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
